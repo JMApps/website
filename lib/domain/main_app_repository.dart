8fa5d4be3e7213ca8fc052383052a2624fa9baf2
dev_muslim <jmappsmuslim@gmail.com>
2023-08-14 14:01:32 +0300

    Delete old code

--- lib/domain/main_app_repository.dart
+++
@@ -1,7 +0,0 @@
-import 'package:website/presentation/model/main_app_model.dart';
-
-abstract class MainAppRepository {
-  Future<List<MainAppModel>> fetchMainApps();
-
-  Future<void> addMainApp(MainAppModel mainAppModel);
-}
